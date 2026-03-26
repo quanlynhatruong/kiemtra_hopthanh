d5ed3a269d3ec6daec2a822780812a9f879e133f
phanmemquanli <tulieuhopthanh@gmail.com>
2026-03-26 20:28:13 +0700

    Update ketnoi.js

--- ketnoi.js
+++ ketnoi.js
@@ -2,60 +2,59 @@
    Tệp: ketnoi.js - Trạm kết nối Github Pages <-> Google Apps Script
    ========================================================================== */
 
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxeQAXxsFNGjJlnnaimCshsK8wLVea9wnVePCR-ioB9Xr3Q4mVVUsDXIUIEYQ1vIZHb/exec"; 
+const HTS_DUONG_DAN_UNG_DUNG_STR = "https://script.google.com/macros/s/AKfycbxeQAXxsFNGjJlnnaimCshsK8wLVea9wnVePCR-ioB9Xr3Q4mVVUsDXIUIEYQ1vIZHb/exec"; 
 
-// Bọc try-catch chống sập hệ thống khi Chrome chặn Storage
-let USER_EMAIL = "";
-try { USER_EMAIL = sessionStorage.getItem("SKT_USER_EMAIL") || ""; } catch(e) { console.warn("Chrome chặn Storage khởi tạo"); }
+let hts_taiKhoanKhachHienTai_str = "";
+try { hts_taiKhoanKhachHienTai_str = sessionStorage.getItem("SKT_USER_ACCOUNT") || sessionStorage.getItem("SKT_USER_EMAIL") || ""; } catch(e) { console.warn("Chrome chặn Storage khởi tạo"); }
 
 const google = {
     script: {
-        run: createRunObject()
+        run: hts_taoDoiTuongChay_fn()
     }
 };
 
-function createRunObject(successCb = null, failureCb = null) {
+function hts_taoDoiTuongChay_fn(hts_hamThanhCong_fn = null, hts_hamThatBai_fn = null) {
     return new Proxy({}, {
         get: function(target, prop) {
-            if (prop === 'withSuccessHandler') return function(cb) { return createRunObject(cb, failureCb); };
-            if (prop === 'withFailureHandler') return function(cb) { return createRunObject(successCb, cb); };
+            if (prop === 'withSuccessHandler') return function(cb) { return hts_taoDoiTuongChay_fn(cb, hts_hamThatBai_fn); };
+            if (prop === 'withFailureHandler') return function(cb) { return hts_taoDoiTuongChay_fn(hts_hamThanhCong_fn, cb); };
             
             return function(...args) {
                 try {
-                    USER_EMAIL = window.SKT_GLOBAL_EMAIL || sessionStorage.getItem("SKT_USER_EMAIL") || "";
+                    hts_taiKhoanKhachHienTai_str = window.hts_taiKhoanToanCuc_str || sessionStorage.getItem("SKT_USER_ACCOUNT") || sessionStorage.getItem("SKT_USER_EMAIL") || "";
                 } catch(e) {
-                    USER_EMAIL = window.SKT_GLOBAL_EMAIL || "";
+                    hts_taiKhoanKhachHienTai_str = window.hts_taiKhoanToanCuc_str || "";
                 }
-                executeBackend(prop, args, successCb, failureCb);
+                hts_thucThiHeThongSau_fn(prop, args, hts_hamThanhCong_fn, hts_hamThatBai_fn);
             };
         }
     });
 }
 
-function executeBackend(action, argsArray, onSuccess, onFailure) {
+function hts_thucThiHeThongSau_fn(action, argsArray, onSuccess, onFailure) {
     let params = {};
-    if (action === "SKT_getRecordByMaHS") params = { maHS: argsArray[0] };
-    else if (action === "SKT_saveRecord") params = { dataArray: argsArray[0] };
-    else if (action === "SKT_submitFinalRecord") params = { dataArray: argsArray[0] };
-    else if (action === "SKT_deleteSingleRowNhatKy") params = { maHS: argsArray[0] };
-    else if (action === "SKT_uploadMultipleFilesToDrive") params = { filesData: argsArray[0], maHoSo: argsArray[1], folderId: argsArray[2], oldUrl: argsArray[3], writeMode: argsArray[4] };
-    else if (action === "SKT_uploadFolderEvidence") params = { parentId: argsArray[0], folderName: argsArray[1], filesData: argsArray[2], oldUrl: argsArray[3], writeMode: argsArray[4] };
-    else if (action === "SKT_searchRecords") params = { criteria: argsArray[0] };
-    else if (action === "SKT_createReportDoc") params = { criteria: argsArray[0] };
-    else if (action === "SKT_getInitialData") params = { email: argsArray[0], ts: argsArray[1] };
-    else if (action === "SKT_getSubFolders") params = {};
-    else if (action === "SKT_getSearchFilterData") params = {};
-    else if (action === "SKT_getUpdatedMaHoSoList") params = {};
+    if (action === "hts_layBanGhiTheoMa_fn") params = { hts_maHSDauVao_str: argsArray[0] };
+    else if (action === "hts_luuTamBanGhi_fn") params = { hts_mangDuLieuDauVao_arr: argsArray[0] };
+    else if (action === "hts_nopKetQuaCuoi_fn") params = { hts_mangDuLieuDauVao_arr: argsArray[0] };
+    else if (action === "hts_xoaMotDongNhatKy_fn") params = { hts_maHSDauVao_str: argsArray[0] };
+    else if (action === "hts_taiNhieuTepLen_fn") params = { hts_duLieuTep_arr: argsArray[0], hts_maHoSoDich_str: argsArray[1], hts_idThuMucDich_str: argsArray[2], hts_duongDanCu_str: argsArray[3], hts_cheDoGhi_str: argsArray[4] };
+    else if (action === "hts_taiThuMucLen_fn") params = { hts_idThuMucCha_str: argsArray[0], hts_tenThuMucMoi_str: argsArray[1], hts_duLieuTep_arr: argsArray[2], hts_duongDanCu_str: argsArray[3], hts_cheDoGhi_str: argsArray[4] };
+    else if (action === "hts_timKiemBanGhi_fn") params = { hts_tieuChiLoc_obj: argsArray[0] };
+    else if (action === "hts_taoTaiLieuBaoCao_fn") params = { hts_tieuChiLoc_obj: argsArray[0] };
+    else if (action === "hts_layDuLieuKhoiTao_fn") params = { account: argsArray[0], hts_chongNhoDem_str: argsArray[1] };
+    else if (action === "hts_layThuMucCon_fn") params = {};
+    else if (action === "hts_layDuLieuLocTimKiem_fn") params = {};
+    else if (action === "hts_layDanhSachMaMoi_fn") params = {};
 
-    fetch(WEB_APP_URL, {
+    fetch(HTS_DUONG_DAN_UNG_DUNG_STR, {
         method: "POST",
         headers: { "Content-Type": "text/plain;charset=utf-8" },
-        body: JSON.stringify({ action: action, params: params, email: USER_EMAIL })
+        body: JSON.stringify({ action: action, params: params, account: hts_taiKhoanKhachHienTai_str, email: hts_taiKhoanKhachHienTai_str })
     })
     .then(res => res.json())
     .then(res => {
-        if (res.status === "success") { if (onSuccess) onSuccess(res.data); } 
-        else { if (onFailure) onFailure(new Error(res.message)); else alert("Lỗi Server: " + res.message); }
+        if (res.hts_thanhCong_bol === "success") { if (onSuccess) onSuccess(res.hts_duLieu_obj); } 
+        else { if (onFailure) onFailure(new Error(res.hts_thongBao_str)); else alert("Lỗi Server: " + res.hts_thongBao_str); }
     })
     .catch(err => {
         if (onFailure) onFailure(err); else console.error("Lỗi Fetch:", err);
