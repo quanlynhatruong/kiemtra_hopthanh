d4e87926dc15a4e942750c44411f5c47917d7eda
phanmemquanli <tulieuhopthanh@gmail.com>
2026-05-14 08:44:31 +0700

    Update ketnoi.js

--- ketnoi.js
+++ ketnoi.js
@@ -5,22 +5,29 @@
  */
 const SKT_GAS_URL = "https://script.google.com/macros/s/AKfycbxeQAXxsFNGjJlnnaimCshsK8wLVea9wnVePCR-ioB9Xr3Q4mVVUsDXIUIEYQ1vIZHb/exec";
 const SKT_LOGO_URL = "https://i.ibb.co/S46wLjDt/logo-tr-ng-TH-THCS-Hop-Thanh3-removebg-preview.png";
+
 const SKT_TEN_TRUONG = "Trường TH&THCS Hợp Thành";
 const SKT_TEN_TRUONG_UP = "TRƯỜNG TH&THCS HỢP THÀNH";
+const SKT_TEN_PM = "Sổ Kiểm Tra Nội Bộ";
+const SKT_TEN_PM_UP = "SỔ KIỂM TRA NỘI BỘ";
+const SKT_TEN_PM_DAY_DU = "PHẦN MỀM KIỂM TRA NỘI BỘ";
+const SKT_PHIEN_BAN = "V3.1.2026";
+const SKT_TAC_GIA = "Hoàng Ngọc Lâm";
+const SKT_TAC_GIA_UP = "HOÀNG NGỌC LÂM";
+
+// MÃ CLIENT ID CỦA GOOGLE ĐĂNG NHẬP
+const SKT_GOOGLE_CLIENT_ID = "407480994586-m6fpq6sfcc90qqj9k08rsmi1lge6br94.apps.googleusercontent.com";
 
 /* ========================================================================= */
 /* KHỞI TẠO CẤU TRÚC HEADER TỰ ĐỘNG TỪ FILE JS                               */
 /* ========================================================================= */
 (function() {
-    // 1. Khởi tạo thẻ bộ mã ký tự (utf-8)
     var metaCharset = document.createElement('meta');
     metaCharset.setAttribute('charset', 'utf-8');
     document.head.appendChild(metaCharset);
 
-    // 2. Thiết lập Tiêu đề trang (Title)
-    document.title = "Sổ Kiểm Tra Nội Bộ - " + SKT_TEN_TRUONG;
+    document.title = SKT_TEN_PM + " - " + SKT_TEN_TRUONG;
 
-    // 3. Khởi tạo thẻ link Icon (Favicon)
     var linkIcon = document.createElement('link');
     linkIcon.rel = 'icon';
     linkIcon.href = SKT_LOGO_URL;
